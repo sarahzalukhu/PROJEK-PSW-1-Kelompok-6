376b5d09ccee510c2a686c39dd7daccaf82dd60d
Andrey157192 <andreyh378@gmail.com>
2024-11-19 11:53:44 +0700

    struktur materi semester 2

---
+++ src/components/semester2/Narative_Text.js
@@ -0,0 +1,22 @@
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import './Exposition.css';
+
+// const App = () => {
+//   return (
+//     <Router>
+//       {/* Navbar akan tampil di semua halaman */}
+//       <Navbar />
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/narrative-text" element={<NarrativeText />} />
+//         <Route path="/procedural-text" element={<ProceduralText />} />
+//         <Route path="/exposition-text" element={<ExpositionText />} />
+//         <Route path="/present-continuous-tense" element={<PresentContinuousTense />} />
+//         <Route path="/report-text" element={<ReportText />} />
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default App;
